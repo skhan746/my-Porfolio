a914c8cbfd29dd8a620c7369ec28986193aca257
skhan746 <shahzorekhan01@gmail.com>
2023-02-03 19:00:29 -0500

    about section update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,25 +6,32 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-[#290033] to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="inline text-4xl font-bold border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
-          Hi, my name is Shahzore Khan and I am currently a student at the University of Western Ontario. I am currently pursuing a Bachelors in Computer Science.
-           My goal is to obtain first hand experience in the workfield via an entry level postion or an internship. 
+        <p className="mt-20 text-xl">
+        Hi, my name is Shahzore Khan and I am a motivated computer science student at the University of Western Ontario. With a passion for software development that stems from 4 years of hands-on learning and practical experience, 
+        I am eager to apply my skills and knowledge in a professional setting. 
         </p>
 
         <br />
 
         <p className="text-xl">
-        I have been learning programming for over 4 years ever since I was in high school and I love it. I am
-very passionate about all aspects of software development and enjoy learning new technologies. Right now I am looking for a position as a front
-end, back end, or full stack developer. I am also open to other positions that could help me expand my network and work experience. feel free to contact me!
+        I am pursuing a Bachelor's degree in Computer Science and my ultimate goal is to gain 
+        hands-on experience through an entry-level position or an internship. I have a strong 
+        foundation in programming and am particularly interested in front-end, back-end, and full-stack 
+        development roles. However, I am also open to exploring other opportunities 
+        in the industry to expand my network and build a diverse range of experience.!
         </p>
+        <br />
+
+        <p className="text-xl">If you are looking for a dedicated and enthusiastic 
+        individual with a passion for technology, don't hesitate to reach out to me. I would love to explore how 
+        I can contribute to your team and further my professional growth.</p>
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,18 +9,18 @@ const Home = () => {
   return (
     
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-[#290033]'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div className='flex flex-col items-center justify-center h-full max-w-screen-lg px-4 mx-auto md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='animate-none text-4xl sm:text-7xl font-bold text-white'>
+                <h2 className='text-4xl font-bold text-white animate-none sm:text-7xl'>
                     I'm a Full Stack Developer
                 </h2>
-                <p className='text-gray-500 py-4 max-w-md'>
+                <p className='max-w-md py-4 text-gray-500'>
                     Welcome to my portfolio! Here you will find all my projects and relevant experience. Feel free to contact me to get in touch. 
                 </p>
                 <div>
-                    <Link to="portfolio" smooth duration={500} offset={-60} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer'>
+                    <Link to="portfolio" smooth duration={500} offset={-60} className='flex items-center px-6 py-3 my-2 text-white rounded-md cursor-pointer group w-fit bg-gradient-to-r from-cyan-500 to to-blue-500'>
                         Portfolio
-                        <span className='group-hover:rotate-90 duration-300'>
+                        <span className='duration-300 group-hover:rotate-90'>
                         <MdOutlineKeyboardArrowRight size={25}
                         className="ml-1" />
                         </span>
@@ -30,12 +30,12 @@ const Home = () => {
 
         
         
-            <div className='animate-pulse'>
+            <div className='animate-bounce'>
 
                 <div>
                     <img src= {HeroImage} 
                     alt="my profile"
-                    className='rounded-2xl mx-auto w-2/3 md:w-full ' />
+                    className='w-2/3 mx-auto rounded-2xl md:w-full ' />
                 </div>
             
             </div>    
